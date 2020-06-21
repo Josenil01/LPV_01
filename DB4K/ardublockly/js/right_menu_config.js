32bd821e5dfac6aea5bd50518976985732f548bc
Paulo Henrique <phvcandido@gmail.com>
2020-06-20 21:33:02 -0300

    Adicionando sweetalert2, adicionando clear

--- DB4K/ardublockly/js/right_menu_config.js
+++ DB4K/ardublockly/js/right_menu_config.js
@@ -4,9 +4,9 @@ el.addEventListener('click', function () {
   // let text = Ardublockly.generateXml();
   // let text = Ardublockly.replaceBLocksfromXml();
   if (typeof text == 'undefined') {
-    swal('Muita calma nessa hora!', 'Você precisa colocar blocos na tela para ver o código', 'warning');
+    swal.fire('Muita calma nessa hora!', 'Você precisa colocar blocos na tela para ver o código', 'warning');
   } else {
-    swal('Seu código', text);
+    swal.fire('Seu código', text);
   }
 });
 // working here ===================================================
@@ -19,3 +19,26 @@ el.addEventListener('click', function () {
 //     swal('Seu código', text);
 //   }
 // });
+
+var elClearBlocks = document.getElementById('btn_clearblocks');
+elClearBlocks.addEventListener('click', function () {
+  swal.fire({
+    title: 'Muita calma nessa hora!',
+    text: 'Você realmente quer deletar todos os blocos da tela?',
+      icon: 'warning',
+      showCancelButton:true,  
+      confirmButtonText: 'Sim, quero remover os blocos',
+      cancelButtonText: 'Não, mudei de ideia'
+  }).then((result) => {
+    if(Ardublockly.isWorkspaceEmpty()){
+      swal.fire('Calma!', 'Vamos programar algo primeiro!', 'warning');
+      return;
+    }
+    if (result.value == true) {
+      Ardublockly.workspace.clear();
+      swal.fire('Feito!', 'Todos os blocos foram removidos!', 'success');
+    }else{
+      swal.fire({title: 'Tudo bem! Vamos continuar', icon: 'success'});
+    }
+  });
+});
